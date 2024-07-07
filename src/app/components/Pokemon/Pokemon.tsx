97e8b8e5c45c46bdf4a0f2c691b251e07b677424
Ahmad Dzaki <ahmad.dzaki99@gmail.com>
2024-07-07 20:11:44 +0800

    first commit

---
+++ src/app/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import usePokemon from "../../hooks/usePokemon";
+import { fetchPokemon } from "../../utils/fetchUtils";
+import PokemonDetails from "../PokemonDetails/PokemonDetails";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+export default function Pokemon() {
+  const { data, isLoading, error } = usePokemon(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+
+  const [pokemonDetails, setPokemonDetails] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      if (data && data.results) {
+        const details = await Promise.all(
+          data.results.map(async (pokemon: Pokemon) => {
+            const pokemonData = await fetchPokemon(pokemon.url);
+            return pokemonData;
+          })
+        );
+        setPokemonDetails(details);
+      }
+    };
+
+    fetchPokemonDetails();
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  //   if (error) {
+  //     return <div>Error: {error.message}</div>;
+  //   }
+
+  return (
+    <>
+      <div className="flex row flex-wrap gap-4 justify-evenly">
+        <PokemonDetails pokemonDetails={pokemonDetails} />
+      </div>
+    </>
+  );
+}
